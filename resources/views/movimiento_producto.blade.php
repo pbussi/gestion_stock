2eabecee55188cb6e69f71bc0a8aa8a27cbc3f08
pbussi@gmail.com <pbussi@gmail.com>
2019-09-30 13:20:49 -0300

    lotes mp

--- resources/views/movimiento_producto.blade.php
+++ resources/views/movimiento_producto.blade.php
@@ -1,5 +1,6 @@
 @extends('layout')
 @section('content')
+ <script src=http://localhost/proyecto/heladeria_rincon/public/vendor/jquery/jquery.min.js></script>
  <div class="container-fluid">
           <!-- Page Heading -->
           <h1 class="h3 mb-2 text-gray-800">Movimientos de Productos</h1>
@@ -17,7 +18,7 @@
               <div class="col-sm-12 mb-3 mb-sm-0 card border-left-info">
                 <p></p>
                 <h5 class="text-info font-weight-bold text-uppercase mb-4"> {{ $dep }} </h5>
-                <table  class="table  table-striped small" id="dataTable_<?php print $pos++ ?>" width="100%" cellspacing="0">
+                <table  class="table  table-striped small" id="dataTable_<?php print $pos; ?>" width="100%" cellspacing="0">
                   <thead>
                     <tr>
                         <th>Fecha</th>
@@ -31,17 +32,11 @@
                  <?php $subtotal=0 ?>
                  @foreach ($movs as $mov)
                 <tr><td>{{date('d-m-Y', strtotime($mov->fecha)) }}</td>
-                  <td> @if ($mov->cantidad<0) Egreso @else Ingreso @endif </td>
+                  <td > @if ($mov->cantidad<0) Egreso @else Ingreso @endif </td>
                   <td>{{$mov->lote }}</td><td>{{date('d-m-Y', strtotime($mov->vencimiento)) }}</td>
-                  <td  @if ($mov->cantidad<0) class='text-danger' @endif ><b>{{$mov->cantidad }}</b></td>
+                  <td @if ($mov->cantidad<0) class='text-danger' @endif ><b>{{$mov->cantidad }}</b></td>
                     <td><?php $subtotal=$subtotal+$mov->cantidad; print $subtotal ?></td></tr>
-                <script>// Call the dataTables jQuery plugin
-                $(document).ready(function() {
-                  $('#dataTable_<?php print $pos ?>').DataTable({
-                  "orderMulti": false
-                });
-                });
-                </script>
+           
                 @endforeach
                  <tfoot>
                     <tr>
@@ -56,6 +51,17 @@
                 </table>
               </div>
     </div>
+
+         <script>// Call the dataTables jQuery plugin
+                $(document).ready(function() {
+                  $('#dataTable_<?php print $pos ?>').DataTable({
+               columnDefs: [
+    { orderable: false, targets: '_all' }
+]
+                });
+                });
+                </script>
+                <?php  $pos++; ?>
    @endforeach
 
 
